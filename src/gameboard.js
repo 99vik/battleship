668aba4780af026c30c2554f60cb5e0766031f3e
Viktor Miric <viktorm47@gmail.com>
2023-08-25 19:41:19 +0200

    Fixed bot bug overflowed stack

--- src/gameboard.js
+++ src/gameboard.js
@@ -5,11 +5,11 @@ const Ship = require('./ship');
 class Gameboard {
   constructor() {
     this.fields = this.generateFields();
-    this.placeShip([2, 2], new Ship(3), 'h');
+    this.placeShip([1, 1], new Ship(3), 'h');
     this.placeShip([5, 5], new Ship(2), 'v');
     this.placeShip([10, 8], new Ship(3), 'h');
     this.placeShip([1, 10], new Ship(4), 'v');
-    this.placeShip([4, 3], new Ship(5), 'v');
+    this.placeShip([6, 1], new Ship(5), 'v');
   }
 
   generateFields() {
--- src/playerAI.js
+++ src/playerAI.js
@@ -31,10 +31,26 @@ class PlayerAI extends Player {
         selectedShot = `${rowNum - 1},${colNum}`;
       } if (playerFields[`${rowNum},${colNum + 1}`] !== 'miss' && playerFields[`${rowNum},${colNum + 1}`] !== 'hit' && colNum + 1 <= 10) {
         selectedShot = `${rowNum},${colNum + 1}`;
-      } if (playerFields[`${rowNum},${colNum - 1}`] !== 'miss' && playerFields[`${rowNum},${colNum - 1}`] !== 'hit' && colNum + 1 >= 1) {
+      } if (playerFields[`${rowNum},${colNum - 1}`] !== 'miss' && playerFields[`${rowNum},${colNum - 1}`] !== 'hit' && colNum - 1 >= 1) {
         selectedShot = `${rowNum},${colNum - 1}`;
       }
     } else {
+      const rowSorted = this.hits
+        .map((coordinate) => [Number(coordinate[0]), Number(coordinate[1])])
+        .sort((a, b) => a[0] - b[0]);
+      const colSorted = this.hits
+        .map((coordinate) => [Number(coordinate[0]), Number(coordinate[1])])
+        .sort((a, b) => a[1] - b[1]);
+      // if (rowSorted[0][0] === rowSorted[1][0]) {
+      //   rowNumFirst = colSorted[0][0];
+      //   colNumFirst = colSorted[0][1];
+      //   colNumSecond = colSorted[colSorted.length - 1][1];
+      // } else {
+      //   colNumFirst = rowSorted[0][0];
+      //   rowNumFirst = rowSorted[0][1];
+      //   rowNumSecond = rowSorted[rowSorted.length - 1][1];
+      // }
+
       const firstField = this.hits[0];
       const secondField = this.hits[this.hits.length - 1];
       const rowNumFirst = Number(firstField[0]);
@@ -42,10 +58,15 @@ class PlayerAI extends Player {
       const rowNumSecond = Number(secondField[0]);
       const colNumSecond = Number(secondField[1]);
       let possibleShots;
+      // if (colNumFirst === colNumSecond) {
+      //   possibleShots = [[`${Math.max(rowNumFirst, rowNumSecond) + 1},${colNumFirst}`], [`${Math.min(rowNumFirst, rowNumSecond) - 1},${colNumFirst}`]];
+      // } else {
+      //   possibleShots = [[`${rowNumFirst},${Math.max(colNumFirst, colNumSecond) + 1}`], [`${rowNumFirst},${Math.min(colNumFirst, colNumSecond) - 1}`]];
+      // }
       if (colNumFirst === colNumSecond) {
-        possibleShots = [[`${Math.max(rowNumFirst, rowNumSecond) + 1},${colNumFirst}`], [`${Math.min(rowNumFirst, rowNumSecond) - 1},${colNumFirst}`]];
+        possibleShots = [[`${Math.max(rowSorted[0][0], rowSorted[rowSorted.length - 1][0]) + 1},${colNumFirst}`], [`${Math.min(rowSorted[0][0], rowSorted[rowSorted.length - 1][0]) - 1},${colNumFirst}`]];
       } else {
-        possibleShots = [[`${rowNumFirst},${Math.max(colNumFirst, colNumSecond) + 1}`], [`${rowNumFirst},${Math.min(colNumFirst, colNumSecond) - 1}`]];
+        possibleShots = [[`${rowNumFirst},${Math.max(colSorted[0][1], colSorted[colSorted.length - 1][1]) + 1}`], [`${rowNumFirst},${Math.min(colSorted[0][1], colSorted[colSorted.length - 1][1]) - 1}`]];
       }
       selectedShot = possibleShots[Math.floor(Math.random() * possibleShots.length)][0];
     }
